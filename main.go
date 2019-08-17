4a14c2c04518ee14f019ed2bf747c4a04f624744
Gowtham Girithar Srirangasamy <gowthamgirithar@gmail.com>
2019-08-17 23:21:38 +0530

    vendor added

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
+// Create a new route and listen on post 8080 for all the requests
+func main() {
+	route := mux.NewRouter()
+	err := http.ListenAndServe(":8080", route)
+	if err != nil {
+		println("Error in running the server ", err.Error())
+		panic(err)
+	}
 }
