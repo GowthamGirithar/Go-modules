bb9084b0005e19844156fb9f7570438836bf1920
Gowtham Girithar Srirangasamy <gowthamgirithar@gmail.com>
2019-09-09 14:50:33 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"employee-service/handlers"
 	"github.com/gorilla/mux"
 	"net/http"
+	"os"
 )
 
 func init() {
@@ -13,9 +14,13 @@ func init() {
 
 // Create a new route and listen on post 8080 for all the requests
 func main() {
+	port := os.Getenv("PORT")
+	if len(port) < 1 {
+		port = "9090"
+	}
 	route := mux.NewRouter()
 	handlers.HandleFunction(route)
-	err := http.ListenAndServe(":8081", route)
+	err := http.ListenAndServe(":"+port, route)
 	if err != nil {
 		println("Error in running the server ", err.Error())
 		panic(err)
